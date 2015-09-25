eaa148d7dfdb05ceea49b773ee31184494c4974b
Luke <luke@madebymade.co.uk>
2015-09-25 20:40:09 +0100

    Update app to use atom for state and change background colour on click

--- src/big_time/core.cljs
+++ src/big_time/core.cljs
@@ -1,10 +1,12 @@
 (ns big-time.core
-  (:require [quiescent.core :refer [render] :refer-macros [defcomponent]]
+  (:require [quiescent.core :as q]
             [quiescent.dom :as dom]
             [goog.dom :as gdom]
             [clojure.string :as string]))
 
 (enable-console-print!)
+
+(def data (atom {:backgrounds [:red :blue :green]}))
 
 (defn time-str [int]
   (if (= (count (str int)) 1)
@@ -16,24 +18,28 @@
    (time-str (.getMinutes date))
    (time-str (.getSeconds date))])
 
-(defcomponent Clock
+(defn change-background [_]
+  (swap! data (fn [data]
+                (update-in data [:backgrounds]
+                  (fn [backgrounds]
+                    (let [new-background (peek backgrounds)]
+                      (apply conj [new-background] (pop backgrounds))))))))
+
+(q/defcomponent Clock
   [date]
-  (dom/div {:className "clock"}
-    (dom/span {:className "clock__hour"} (get date 0))
-    (dom/span {:className "clock__minute"} ":" (get date 1))
-    (dom/span {:className "clock__second"} ":" (get date 2))))
+  (dom/div {:className "clock"
+            :style {:background (first (:backgrounds @data))}
+            :onClick change-background}
+    (dom/div {:className "clock__time"}
+      (dom/span {:className "clock__hour"} (get date 0))
+      (dom/span {:className "clock__minute"} ":" (get date 1))
+      (dom/span {:className "clock__second"} ":" (get date 2)))))
 
-(defn set-doc-title [title]
-  (set! (.-title js/document) title))
-
-(defn render-clock [date]
-  (render (Clock date) (gdom/getElement "app")))
-
-(defn tick []
+(defn render []
   (let [date (current-time (js/Date.))]
-    (set-doc-title (string/join ":" date))
-    (render-clock date)
-    (js/setTimeout tick 500)))
+    (set! (.-title js/document) (string/join ":" date))
+    (q/render (Clock date) (gdom/getElement "app"))))
 
 (defn init []
-  (tick))
+  (render)
+  (.requestAnimationFrame js/window init))
