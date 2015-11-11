2d3c674e00a6134b95911919512a97a12cba9c85
Luke Morton <lukemorton.dev@gmail.com>
2015-11-11 21:45:10 +0000

    Move init into fn

--- src/big_time/core.cljs
+++ src/big_time/core.cljs
@@ -13,50 +13,51 @@
 
 (enable-console-print!)
 
-; Default application state
-;
-(def data-atom (atom {:backgrounds [:#79BD9A :#3B8686 :#0B486B]
-                      :countdown {:form {:hours "00" :minutes "10" :seconds "00"}
-                                  :duration 0
-                                  :start-time nil
-                                  :current-time ["00" "00" "00"]}
-                      :current-time ["00" "00" "00"]
-                      :page nil
-                      :pages {:time time/Time
-                              :countdown countdown/Countdown
-                              :about pages/About}
-                      :path nil}))
+(defn init []
+  ; Default application state
+  ;
+  (def data-atom (atom {:backgrounds [:#79BD9A :#3B8686 :#0B486B]
+                        :countdown {:form {:hours "00" :minutes "10" :seconds "00"}
+                                    :duration 0
+                                    :start-time nil
+                                    :current-time ["00" "00" "00"]}
+                        :current-time ["00" "00" "00"]
+                        :page nil
+                        :pages {:time time/Time
+                                :countdown countdown/Countdown
+                                :about pages/About}
+                        :path nil}))
 
-; Just for logging application state changes
-;
-; (add-watch data-atom :path-logger
-;   (fn [key data-atom state next-state]
-;     (println next-state)))
+  ; Just for logging application state changes
+  ;
+  ; (add-watch data-atom :path-logger
+  ;   (fn [key data-atom state next-state]
+  ;     (println next-state)))
 
-; Rebuilds app everytime state changes provided it can find a page
-; component to render.
-;
-(add-watch data-atom :re-renderer
-  (fn [key data-atom state next-state]
-    (if-let [page-component (:page next-state)]
-      (q/render (app/App @data-atom page-component data-atom) (gdom/getElement "app")))))
+  ; Rebuilds app everytime state changes provided it can find a page
+  ; component to render.
+  ;
+  (add-watch data-atom :re-renderer
+    (fn [key data-atom state next-state]
+      (if-let [page-component (:page next-state)]
+        (q/render (app/App @data-atom page-component data-atom) (gdom/getElement "app")))))
 
-; Everytime application state :path changes we dispatch secretary.
-; Secretary will then update application state :page-component
-; which represents main page component for that URL.
-;
-(add-watch data-atom :path-dispatcher
-  (fn [key data-atom state next-state]
-    (if (not= (:path state) (:path next-state))
-      (if-let [page-component (get (:pages next-state) (routes/path->component (:path next-state)))]
-        (swap! data-atom assoc :page page-component)))))
+  ; Everytime application state :path changes we dispatch secretary.
+  ; Secretary will then update application state :page-component
+  ; which represents main page component for that URL.
+  ;
+  (add-watch data-atom :path-dispatcher
+    (fn [key data-atom state next-state]
+      (if (not= (:path state) (:path next-state))
+        (if-let [page-component (get (:pages next-state) (routes/path->component (:path next-state)))]
+          (swap! data-atom assoc :page page-component)))))
 
-; History will update application state :path everytime URL changes
-;
-(let [history (History.)]
-  (events/listen history EventType.NAVIGATE
-    (fn [e]
-      (let [token (.-token e)]
-        (swap! data-atom assoc :path (if (empty? token) "/" token)))))
+  ; History will update application state :path everytime URL changes
+  ;
+  (let [history (History.)]
+    (events/listen history EventType.NAVIGATE
+      (fn [e]
+        (let [token (.-token e)]
+          (swap! data-atom assoc :path (if (empty? token) "/" token)))))
 
-  (.setEnabled history true))
+    (.setEnabled history true)))
