4cbdc2bc19284a0e00d4de4e4427774b394bcdcd
Luke Morton <lukemorton.dev@gmail.com>
2015-11-21 18:39:05 +0000

    Remove background changing

--- src/big_time/ui/app.cljs
+++ src/big_time/ui/app.cljs
@@ -3,19 +3,10 @@
             [quiescent.dom :as dom]
             [big-time.routes :as routes]))
 
-(defn- deal-next-background [backgrounds]
-  (let [new-background (peek backgrounds)]
-    (apply conj [new-background] (pop backgrounds))))
-
-(defn- change-background [data-atom _]
-  (swap! data-atom update-in [:backgrounds] deal-next-background))
-
 (q/defcomponent App
   :name "App"
   [data page-component data-atom]
-  (dom/div {:className "app"
-            :style {:background (first (:backgrounds data))}
-            :onClick (partial change-background data-atom)}
+  (dom/div {:className "app"}
     (dom/nav {:className "app__nav"}
       (dom/a {:href (routes/component->path :clock)} "clock")
       (dom/a {:href (routes/component->path :countdown)} "countdown")
