e229ddf911e82c38ce7cd235fde2a5508db9ae9c
Luke Morton <lukemorton.dev@gmail.com>
2015-11-24 20:36:18 +0000

    Bit o doc

--- src/cljs/big_time/worker.cljs
+++ src/cljs/big_time/worker.cljs
@@ -1,9 +1,14 @@
 (ns big-time.worker)
 
-(defn- process [tasks]
+(defn- process
+  "Maps over tasks, an array of functions"
+  [tasks]
   (doall (map (fn [task] (task)) tasks)))
 
-(defn create [find-fn]
+(defn create
+  "Creates a new worker that executes an array of functions returned from
+   find-fn every 500 milliseconds"
+  [find-fn]
   (letfn [(process-tick []
             (process (find-fn))
             (js/setTimeout process-tick 500))]
